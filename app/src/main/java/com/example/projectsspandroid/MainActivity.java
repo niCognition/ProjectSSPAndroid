00603600b95bcf8303a5a7672b559f81987eb649
Herman Singh <hermansingh1993@gmail.com>
2020-03-24 10:37:25 +0100

    bruh

--- app/src/main/java/com/example/projectsspandroid/MainActivity.java
+++ app/src/main/java/com/example/projectsspandroid/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.projectsspandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
     }
+
+
 }
