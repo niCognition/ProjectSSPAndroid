abcf975058335c8b0e76e0ba495344368fed34eb
Herman Singh <hermansingh1993@gmail.com>
2020-03-24 11:49:21 +0100

    bruh

--- app/src/main/java/com/example/projectsspandroid/LoginScreen.java
+++ app/src/main/java/com/example/projectsspandroid/LoginScreen.java
@@ -1,6 +1,13 @@
 package com.example.projectsspandroid;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LoginScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle SavedInstance) {
+        super.onCreate(SavedInstance);
+        setContentView(R.layout.activity_login_screen);
+    }
 }
--- app/src/main/java/com/example/projectsspandroid/MainActivity.java
+++ app/src/main/java/com/example/projectsspandroid/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.projectsspandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button splashButton;
 
 
     @Override
@@ -13,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        splashButton = (Button) findViewById(R.id.splashButton);
+
+
+        splashButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, LoginScreen.class));
+            }
+        });
 
     }
 
